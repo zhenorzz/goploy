f17030d22a90ab0d3be746be9297c7a687aba193
张臻 <zhangzhen@tanwan.com>
2019-07-21 10:23:08 +0800

    U sql

--- goploy.sql
+++ goploy.sql
@@ -1,190 +1,234 @@
-﻿# Host: localhost  (Version: 5.5.53)
-# Date: 2019-07-15 16:11:00
-# Generator: MySQL-Front 5.3  (Build 4.234)
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET NAMES utf8 */;
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50553
+ Source Host           : localhost:3306
+ Source Schema         : goploy
 
-#
-# Structure for table "git_trace"
-#
+ Target Server Type    : MySQL
+ Target Server Version : 50553
+ File Encoding         : 65001
 
+ Date: 21/07/2019 10:15:53
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+CREATE DATABASE IF NOT EXISTS `goploy`;
+-- ----------------------------
+-- Table structure for git_trace
+-- ----------------------------
 DROP TABLE IF EXISTS `git_trace`;
-CREATE TABLE `git_trace` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `project_id` int(10) unsigned NOT NULL,
-  `project_name` varchar(255) NOT NULL,
-  `detail` text NOT NULL,
-  `state` tinyint(4) unsigned NOT NULL,
-  `publisher_id` int(10) unsigned NOT NULL,
-  `publisher_name` varchar(255) NOT NULL,
-  `create_time` int(10) unsigned NOT NULL,
-  `update_time` int(10) unsigned NOT NULL,
+CREATE TABLE `git_trace`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `commit` char(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `project_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `project_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `detail` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `state` tinyint(4) UNSIGNED NOT NULL DEFAULT 1,
+  `publisher_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `publisher_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `project_id` (`project_id`) USING BTREE COMMENT 'project_id'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  INDEX `project_id`(`project_id`) USING BTREE COMMENT 'project_id'
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for table "log"
-#
+-- ----------------------------
+-- Records of git_trace
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for log
+-- ----------------------------
 DROP TABLE IF EXISTS `log`;
-CREATE TABLE `log` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '日志类型',
-  `ip` int(10) unsigned NOT NULL DEFAULT '0',
-  `desc` varchar(30) NOT NULL DEFAULT '' COMMENT '备注',
-  `user_id` int(10) unsigned NOT NULL COMMENT '用户ID',
-  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+CREATE TABLE `log`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `type` tinyint(3) UNSIGNED NOT NULL DEFAULT 1 COMMENT '日志类型',
+  `ip` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `desc` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
+  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户ID',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `create_time` (`create_time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  INDEX `create_time`(`create_time`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for table "permission"
-#
-
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
 DROP TABLE IF EXISTS `permission`;
-CREATE TABLE `permission` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '权限id',
-  `title` varchar(64) NOT NULL DEFAULT '' COMMENT '权限标题',
-  `uri` varchar(1000) NOT NULL DEFAULT '' COMMENT '权限uri',
-  `state` tinyint(1) NOT NULL DEFAULT '1' COMMENT '该记录是否有效1：有效、0：无效',
-  `pid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父级权限ID',
+CREATE TABLE `permission`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '权限id',
+  `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '权限标题',
+  `uri` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '权限uri',
+  `state` tinyint(1) NOT NULL DEFAULT 1 COMMENT '该记录是否有效1：有效、0：无效',
+  `pid` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '父级权限ID',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
 INSERT INTO `permission` VALUES (1, '主页', '/dashboard', 1, 0);
 INSERT INTO `permission` VALUES (2, '主页列表', '/dashboard/list', 1, 1);
 INSERT INTO `permission` VALUES (3, '项目', '/project', 1, 0);
-INSERT INTO `permission` VALUES (4, '项目部署', '/project/deploy', 1, 3);
-INSERT INTO `permission` VALUES (5, '项目设置', '/project/setting', 1, 3);
-INSERT INTO `permission` VALUES (6, '项目详情', '/project/detail', 1, 3);
-INSERT INTO `permission` VALUES (7, '服务器管理', '/project/server', 1, 3);
-INSERT INTO `permission` VALUES (8, '成员', '/member', 1, 0);
-INSERT INTO `permission` VALUES (9, '成员列表', '/member/list', 1, 8);
-INSERT INTO `permission` VALUES (10, '部署', '/deploy', 1, 0);
-INSERT INTO `permission` VALUES (11, '构建发布', '/deploy/publish', 1, 10);
-#
-# Structure for table "project"
-#
+INSERT INTO `permission` VALUES (4, '项目设置', '/project/setting', 1, 3);
+INSERT INTO `permission` VALUES (5, '服务器管理', '/project/server', 1, 3);
+INSERT INTO `permission` VALUES (6, '成员', '/member', 1, 0);
+INSERT INTO `permission` VALUES (7, '成员列表', '/member/list', 1, 6);
+INSERT INTO `permission` VALUES (8, '部署', '/deploy', 1, 0);
+INSERT INTO `permission` VALUES (9, '构建发布', '/deploy/publish', 1, 8);
 
+-- ----------------------------
+-- Table structure for project
+-- ----------------------------
 DROP TABLE IF EXISTS `project`;
-CREATE TABLE `project` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL COMMENT '项目名称',
-  `url` varchar(255) NOT NULL COMMENT '项目仓库地址',
-  `path` varchar(255) NOT NULL COMMENT '项目部署路径',
-  `script` varchar(255) NOT NULL COMMENT '脚本路径',
-  `rsync_option` varchar(255) NOT NULL COMMENT 'rsync 参数',
-  `publisher_id` int(10) unsigned NOT NULL,
-  `publisher_name` varchar(255) NOT NULL,
-  `create_time` int(11) unsigned NOT NULL,
-  `update_time` int(11) unsigned NOT NULL,
+CREATE TABLE `project`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '项目名称',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '项目仓库地址',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '项目部署路径',
+  `script` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '脚本路径',
+  `rsync_option` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'rsync 参数',
+  `state` tinyint(4) UNSIGNED NOT NULL DEFAULT 1 COMMENT '0=>失效 1=>生效',
+  `publisher_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `publisher_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `publish_state` tinyint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '0=>失效 1=>生效',
+  `create_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `update_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for table "project_server"
-#
+-- ----------------------------
+-- Records of project
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for project_server
+-- ----------------------------
 DROP TABLE IF EXISTS `project_server`;
-CREATE TABLE `project_server` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `project_id` int(10) unsigned NOT NULL,
-  `server_id` int(10) unsigned NOT NULL,
-  `create_time` int(11) unsigned NOT NULL,
-  `update_time` int(11) unsigned NOT NULL,
+CREATE TABLE `project_server`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `project_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `server_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `create_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `update_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `project_id` (`project_id`,`server_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  UNIQUE INDEX `project_id`(`project_id`, `server_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for table "project_user"
-#
+-- ----------------------------
+-- Records of project_server
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for project_user
+-- ----------------------------
 DROP TABLE IF EXISTS `project_user`;
-CREATE TABLE `project_user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `project_id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `create_time` int(11) unsigned NOT NULL,
-  `update_time` int(11) unsigned NOT NULL,
+CREATE TABLE `project_user`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `project_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `create_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `update_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `project_id` (`project_id`,`user_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  UNIQUE INDEX `project_id`(`project_id`, `user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for table "remote_trace"
-#
+-- ----------------------------
+-- Records of project_user
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for remote_trace
+-- ----------------------------
 DROP TABLE IF EXISTS `remote_trace`;
-CREATE TABLE `remote_trace` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `git_trace_id` int(10) unsigned NOT NULL,
-  `project_id` int(10) unsigned NOT NULL,
-  `project_name` varchar(255) NOT NULL,
-  `server_id` int(11) unsigned NOT NULL,
-  `server_name` varchar(255) NOT NULL,
-  `detail` text NOT NULL,
-  `state` tinyint(4) unsigned NOT NULL,
-  `publisher_id` int(10) unsigned NOT NULL,
-  `publisher_name` varchar(255) NOT NULL,
-  `type` tinyint(3) unsigned NOT NULL COMMENT '1同步文件2运行脚本',
-  `create_time` int(10) unsigned NOT NULL,
-  `update_time` int(10) unsigned NOT NULL,
+CREATE TABLE `remote_trace`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `git_trace_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `project_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `project_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `server_id` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `server_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `detail` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `state` tinyint(4) UNSIGNED NOT NULL DEFAULT 1,
+  `publisher_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `publisher_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `type` tinyint(3) UNSIGNED NOT NULL DEFAULT 1 COMMENT '1同步文件2运行脚本',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `project_id` (`project_id`) USING BTREE COMMENT 'project_id'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  INDEX `project_id`(`project_id`) USING BTREE COMMENT 'project_id'
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for table "role"
-#
+-- ----------------------------
+-- Records of remote_trace
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
 DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '角色id',
-  `name` varchar(64) NOT NULL DEFAULT '' COMMENT '角色名',
-  `permission_list` varchar(255) NOT NULL COMMENT '权限列表',
-  `state` tinyint(1) NOT NULL DEFAULT '1' COMMENT '该记录是否有效1：有效、0：无效',
-  `create_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
-  `update_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
-  `desc` varchar(100) NOT NULL DEFAULT '' COMMENT '备注',
+CREATE TABLE `role`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '角色id',
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '角色名',
+  `permission_list` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '权限列表',
+  `state` tinyint(1) NOT NULL DEFAULT 1 COMMENT '该记录是否有效1：有效、0：无效',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建时间',
+  `update_time` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '更新时间',
+  `desc` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `role` VALUES (1, '超级管理员', '1,2,3,4,5,6,7,8,9,10,11', 1, 1527927486, 1527927486, '超级管理员是最高权限者');
-INSERT INTO `role` VALUES (2, '管理员', '10,11', 1, 1527928486, 1552642448, '包含除呼叫功能以外所有客服和管理权限');
-INSERT INTO `role` VALUES (3, '普通客服', '10,11', 1, 1527928486, 1552979330, '负责一线在线咨询的接待, 留言的处理(若购买工单中心, 则还有工单功能, 未购买则无)');
-INSERT INTO `role` VALUES (4, '高级管理员', '10,11', 1, 1546413701, 1551148083, '大佬就是不一样');
-#
-# Structure for table "server"
-#
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, '超级管理员', '1,2,3,4,5,6,7,8,9', 1, 1527927486, 1527927486, '超级管理员是最高权限者');
+INSERT INTO `role` VALUES (2, '管理员', '3,4,5,7,8,9', 1, 1527928486, 1552642448, '包含除成员管理以外所有权限');
+INSERT INTO `role` VALUES (3, '组员', '8,9', 1, 1527928486, 1552979330, '只能发布项目');
+
+-- ----------------------------
+-- Table structure for server
+-- ----------------------------
 DROP TABLE IF EXISTS `server`;
-CREATE TABLE `server` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `ip` varchar(255) NOT NULL,
-  `owner` varchar(255) NOT NULL,
-  `create_time` int(10) unsigned NOT NULL,
-  `update_time` int(11) unsigned NOT NULL,
+CREATE TABLE `server`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `owner` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `create_time` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `update_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `state` tinyint(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT '0=>失效 1=>生效',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
-#
-# Structure for table "user"
-#
+-- ----------------------------
+-- Records of server
+-- ----------------------------
 
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `account` varchar(30) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `mobile` varchar(15) NOT NULL,
-  `role_id` int(10) unsigned NOT NULL,
-  `state` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0:=被禁用  1=正常',
-  `create_time` int(11) unsigned NOT NULL,
-  `update_time` int(11) unsigned NOT NULL,
-  `last_login_time` int(11) unsigned NOT NULL COMMENT '最后登陆时间',
+CREATE TABLE `user`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `account` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `mobile` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `role_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `state` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0:=被禁用  1=正常',
+  `create_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `update_time` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `last_login_time` int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后登陆时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-INSERT INTO `user` VALUES (1, 'admin', '$2a$10$89ZJ2xeJj35GOw11Qiucr.phaEZP4.kBX6aKTs7oWFp1xcGBBgijm', '超管', '18034562122', 1, 1, 1540869597, 1561461294, 1562320680);
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'admin', '$2a$10$89ZJ2xeJj35GOw11Qiucr.phaEZP4.kBX6aKTs7oWFp1xcGBBgijm', '超管', '18023496666', 1, 1, 1540869597, 1561461294, 1563668841);
+
+SET FOREIGN_KEY_CHECKS = 1;
