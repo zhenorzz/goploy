6bf3a232383bee62b410a5eb92a046f067397d61
张臻 <zhangzhen@tanwan.com>
2019-08-28 11:07:02 +0800

    A goploy

--- model/RoleModel.go
+++ model/RoleModel.go
@@ -1,9 +1,5 @@
 package model
 
-import (
-	"errors"
-)
-
 // Role mysql table server
 type Role struct {
 	ID             uint32 `json:"id"`
@@ -22,7 +18,7 @@ func (r Role) GetData() (Role, error) {
 	var role Role
 	err := DB.QueryRow("SELECT id, name, permission_list FROM role WHERE id = ?", r.ID).Scan(&role.ID, &role.Name, &role.PermissionList)
 	if err != nil {
-		return role, errors.New("数据查询失败")
+		return role, err
 	}
 	return role, nil
 }
