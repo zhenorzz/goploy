96b14d3246e1a35eb10f2f90f3d7c55089895a55
张臻 <zhangzhen@tanwan.com>
2019-08-30 10:44:39 +0800

    A js

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -167,7 +167,7 @@ export function deepClone(item) {
  */
 export function humanSize(bytes) {
   if (bytes === 0) return '0 B'
-  var k = 1000 // or 1024
+  var k = 1024 // or 1024
   var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   var i = Math.floor(Math.log(bytes) / Math.log(k))
 
