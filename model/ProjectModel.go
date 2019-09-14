daa4d1e1d9136072eeb7794509fcdec8068b0138
zhangzhen <zhangzhen@tanwan.com>
2019-09-14 12:58:51 +0800

    A query builder

--- model/ProjectModel.go
+++ model/ProjectModel.go
@@ -1,9 +1,12 @@
 package model
 
 import (
-	"errors"
-	"time"
+	"strings"
+
+	sq "github.com/Masterminds/squirrel"
 )
+
+const projectTable = "`project`"
 
 // Project mysql table project
 type Project struct {
@@ -31,32 +34,12 @@ type Projects []Project
 
 // AddRow add one row to table project and add id to p.ID
 func (p Project) AddRow() (int64, error) {
-	result, err := DB.Exec(
-		`INSERT INTO project (
-			group_id,
-			name, 
-			url, 
-			path, 
-			environment, 
-			branch, 
-			after_pull_script, 
-			after_deploy_script, 
-			rsync_option, 
-			create_time, 
-			update_time) 
-		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-		p.GroupID,
-		p.Name,
-		p.URL,
-		p.Path,
-		p.Environment,
-		p.Branch,
-		p.AfterPullScript,
-		p.AfterDeployScript,
-		p.RsyncOption,
-		p.CreateTime,
-		p.UpdateTime,
-	)
+	result, err := sq.
+		Insert(projectTable).
+		Columns("group_id", "name", "url", "path", "environment", "branch", "after_pull_script", "after_deploy_script", "rsync_option", "create_time", "update_time").
+		Values(p.GroupID, p.Name, p.URL, p.Path, p.Environment, p.Branch, p.AfterPullScript, p.AfterDeployScript, p.RsyncOption, p.CreateTime, p.UpdateTime).
+		RunWith(DB).
+		Exec()
 	if err != nil {
 		return 0, err
 	}
@@ -66,64 +49,66 @@ func (p Project) AddRow() (int64, error) {
 
 // EditRow edit one row to table project
 func (p Project) EditRow() error {
-	_, err := DB.Exec(
-		`UPDATE project SET 
-		  group_id = ?,
-		  name = ?,
-		  url = ?,
-		  path = ?,
-		  environment = ?,
-		  branch = ?,
-		  after_pull_script = ?,
-		  after_deploy_script = ?,
-		  rsync_option = ?
-		WHERE
-		  id = ?`,
-		p.GroupID,
-		p.Name,
-		p.URL,
-		p.Path,
-		p.Environment,
-		p.Branch,
-		p.AfterPullScript,
-		p.AfterDeployScript,
-		p.RsyncOption,
-		p.ID,
-	)
+	_, err := sq.
+		Update(projectTable).
+		SetMap(sq.Eq{
+			"group_id":            p.GroupID,
+			"name":                p.Name,
+			"url":                 p.URL,
+			"path":                p.Path,
+			"environment":         p.Environment,
+			"branch":              p.Branch,
+			"after_pull_script":   p.AfterPullScript,
+			"after_deploy_script": p.AfterDeployScript,
+			"rsync_option":        p.RsyncOption,
+			"update_time":         p.UpdateTime,
+		}).
+		Where(sq.Eq{"id": p.ID}).
+		RunWith(DB).
+		Exec()
 	return err
 }
 
 // RemoveRow project
 func (p Project) RemoveRow() error {
-	_, err := DB.Exec(
-		`UPDATE project SET 
-		  state = ?,
-		  update_time = ?
-		WHERE
-		 id = ?`,
-		Disable,
-		p.UpdateTime,
-		p.ID,
-	)
+	_, err := sq.
+		Update(projectTable).
+		SetMap(sq.Eq{
+			"state":       Disable,
+			"update_time": p.UpdateTime,
+		}).
+		Where(sq.Eq{"id": p.ID}).
+		RunWith(DB).
+		Exec()
 	return err
 }
 
 // Publish for project
 func (p Project) Publish() error {
-	_, err := DB.Exec(
-		"UPDATE project SET publisher_id = ?, publisher_name = ?, last_publish_token = ?, update_time = ? WHERE id = ?",
-		p.PublisherID,
-		p.PublisherName,
-		p.LastPublishToken,
-		p.UpdateTime,
-		p.ID,
-	)
+	_, err := sq.
+		Update(projectTable).
+		SetMap(sq.Eq{
+			"publisher_id":       p.PublisherID,
+			"publisher_name":     p.PublisherName,
+			"last_publish_token": p.LastPublishToken,
+			"update_time":        p.UpdateTime,
+		}).
+		Where(sq.Eq{"id": p.ID}).
+		RunWith(DB).
+		Exec()
 	return err
 }
 
 // GetList project row
 func (p Project) GetList() (Projects, error) {
-	rows, err := DB.Query("SELECT id, group_id, name, url, path, environment, branch, after_pull_script, after_deploy_script, rsync_option, create_time, update_time FROM project WHERE state = 1 ORDER BY id DESC")
+	rows, err := sq.
+		Select("id, group_id, name, url, path, environment, branch, after_pull_script, after_deploy_script, rsync_option, create_time, update_time").
+		From(projectTable).
+		Where(sq.Eq{"state": Enable}).
+		OrderBy("id DESC").
+		RunWith(DB).
+		Query()
+
 	if err != nil {
 		return nil, err
 	}
@@ -141,14 +126,18 @@ func (p Project) GetList() (Projects, error) {
 
 // GetListByManagerGroupStr project row
 func (p Project) GetListByManagerGroupStr(managerGroupStr string) (Projects, error) {
-	sql := "SELECT id, group_id, name, url, path, environment, branch, after_pull_script, after_deploy_script, rsync_option, create_time, update_time FROM project WHERE state = 1"
 	if managerGroupStr == "" {
 		return nil, nil
-	} else if managerGroupStr != "all" {
-		sql += " AND group_id IN (" + managerGroupStr + ")"
 	}
-	sql += " ORDER BY id DESC"
-	rows, err := DB.Query(sql)
+	builer := sq.
+		Select("id, group_id, name, url, path, environment, branch, after_pull_script, after_deploy_script, rsync_option, create_time, update_time").
+		From(projectTable).
+		Where(sq.Eq{"state": Enable}).
+		OrderBy("id DESC")
+	if managerGroupStr != "all" {
+		builer.Where(sq.Eq{"group_id": strings.Split(managerGroupStr, ",")})
+	}
+	rows, err := builer.RunWith(DB).Query()
 	if err != nil {
 		return nil, err
 	}
@@ -167,27 +156,16 @@ func (p Project) GetListByManagerGroupStr(managerGroupStr string) (Projects, err
 // GetData add project information to p *Project
 func (p Project) GetData() (Project, error) {
 	var project Project
-	err := DB.QueryRow("SELECT id, name, url, path, environment, branch, after_pull_script, after_deploy_script, rsync_option, create_time, update_time FROM project WHERE id = ?", p.ID).Scan(&project.ID, &project.Name, &project.URL, &project.Path, &project.Environment, &project.Branch, &project.AfterPullScript, &project.AfterDeployScript, &project.RsyncOption, &project.CreateTime, &project.UpdateTime)
+	err := sq.
+		Select("id, group_id, name, url, path, environment, branch, after_pull_script, after_deploy_script, rsync_option, create_time, update_time").
+		From(projectTable).
+		Where(sq.Eq{"id": p.ID}).
+		OrderBy("id DESC").
+		RunWith(DB).
+		QueryRow().
+		Scan(&project.ID, &project.Name, &project.URL, &project.Path, &project.Environment, &project.Branch, &project.AfterPullScript, &project.AfterDeployScript, &project.RsyncOption, &project.CreateTime, &project.UpdateTime)
 	if err != nil {
-		return project, errors.New("数据查询失败")
+		return project, err
 	}
 	return project, nil
-}
-
-// FindNeedToUpdateProjectList find the project need to update its publish state
-func (p Project) FindNeedToUpdateProjectList() (Projects, error) {
-	rows, err := DB.Query("SELECT id, name, url, path, after_pull_script, after_deploy_script, rsync_option, create_time, update_time FROM project WHERE update_time >= ?", time.Now().Unix()-30*60)
-	if err != nil {
-		return nil, err
-	}
-	var projects Projects
-	for rows.Next() {
-		var project Project
-
-		if err := rows.Scan(&project.ID, &project.Name, &project.URL, &project.Path, &project.AfterPullScript, &project.AfterDeployScript, &project.RsyncOption, &project.CreateTime, &project.UpdateTime); err != nil {
-			return nil, err
-		}
-		projects = append(projects, project)
-	}
-	return projects, nil
 }
