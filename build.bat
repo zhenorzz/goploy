b6799894af53daaa5b1e3e547beba59064a78bca
张臻 <zhangzhen@tanwan.com>
2019-11-30 16:02:16 +0800

    A build mac

--- build.bat
+++ build.bat
@@ -3,6 +3,11 @@ SET GOOS=linux
 SET GOARCH=amd64
 go build -o goploy main.go
 
+::go build mac
+SET GOOS=darwin
+SET GOARCH=amd64
+go build -o goploy.mac main.go
+
 ::go build windows
 SET GOOS=windows
 SET GOARCH=amd64
