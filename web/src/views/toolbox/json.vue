0cec1a451182a85ae85548178dbf925383f454e9
张臻 <zhangzhen@tanwan.com>
2019-12-07 19:11:44 +0800

    A json

--- web/src/views/toolbox/json.vue
+++ web/src/views/toolbox/json.vue
@@ -1,9 +1,16 @@
 <template>
   <el-row class="tree-container">
     <el-col :span="10" class="input-container">
-      <div ref="jsonStringInput" class="json-string-input" contenteditable="true" placeholder="在此输入json字符串" @input="handleInput" />
+      <el-input ref="jsonStringInput" v-model="inputContent" :autosize="false" type="textarea" class="json-string-input" contenteditable="true" placeholder="在此输入json字符串" @input="handleInput" />
     </el-col>
     <el-col :span="14" class="json-container">
+      <div class="json-helper">
+        <el-button type="text" size="medium" @click="expandAll">展开所有</el-button>
+        <el-button type="text" size="medium" @click="collapseAll">收起所有</el-button>
+        <el-button type="text" size="medium">高亮查找</el-button>
+        <el-button type="text" size="medium">取消高亮</el-button>
+        <el-button type="text" size="medium">查看JSON</el-button>
+      </div>
       <div ref="jsonPrettyString" />
     </el-col>
   </el-row>
@@ -16,6 +23,7 @@ export default {
 
   data() {
     return {
+      inputContent: '',
       tree: undefined
     }
   },
@@ -37,9 +45,9 @@ export default {
     // tree.expand()
   },
   methods: {
-    handleInput: debounce(function(event) {
+    handleInput: debounce(function() {
       const wrapper = this.$refs.jsonPrettyString
-      const text = event.target.innerText.replace(/<[^>]+>/g, '')
+      const text = this.inputContent
       if (text.length === 0) {
         wrapper.innerText = ''
         return
@@ -56,7 +64,15 @@ export default {
       } catch (error) {
         wrapper.innerText = error.message
       }
-    })
+    }),
+
+    expandAll() {
+      this.tree && this.tree.expand()
+    },
+
+    collapseAll() {
+      this.tree && this.tree.collapse()
+    }
   }
 }
 </script>
@@ -67,19 +83,24 @@ export default {
     height: 100%;
   }
   .json-string-input {
-    padding: 20px 30px;
     height: 100%;
-    &:empty:before{
-      content: attr(placeholder);
-      color:#bbb;
-    }
-    &:focus:before{
-      content:none;
+    >>> textarea {
+      border-radius: 0;
+      padding: 20px 30px;
+      height: 100%;
+      resize: none;
     }
   }
+
+  .json-helper {
+    padding: 0 10px;
+    height: 35px;
+    border-bottom: solid 1px #EBEEF5;
+  }
+
   .json-container {
     height: 100%;
-    border-left: solid 1px #f6f6f6;
+    border-left: solid 1px #EBEEF5;
   }
 }
 
