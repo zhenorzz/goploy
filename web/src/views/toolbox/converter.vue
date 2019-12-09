8635e9ef2706becbc68c9d5f93e57dd4f5f664cb
张臻 <zhangzhen@tanwan.com>
2019-12-09 20:00:06 +0800

    A 字节转换

--- web/src/views/toolbox/converter.vue
+++ web/src/views/toolbox/converter.vue
@@ -1,22 +1,35 @@
 <template>
   <el-row class="app-container">
     <el-row>
-      <span style="font-size:14px;margin-right:10px">时间戳</span>
+      <span style="display:inline-block;width:60px;font-size:14px;margin-right:10px">时间戳</span>
       <el-input v-model="timestamp" style="width: 200px" />
       <el-button type="primary" @click="timestampToDate">转换>></el-button>
       <el-input v-model="date" style="width: 200px" />
-      <span style="font-size:14px;margin-left:10px;">北京时间</span>
+    </el-row>
+    <el-row style="margin-top: 10px">
+      <span style="display:inline-block;width:60px;font-size:14px;margin-right:10px">字节</span>
+      <el-input v-model="bytes" style="width: 200px" />
+      <el-select v-model="bytesUnit" style="width: 70px">
+        <el-option :value="1" label="B" />
+        <el-option :value="1*1024" label="KB" />
+        <el-option :value="1024*1024" label="MB" />
+      </el-select>
+      <el-button type="primary" @click="bytesToHumanSize">转换>></el-button>
+      <el-input v-model="humanSize" style="width: 200px" />
     </el-row>
   </el-row>
 </template>
 <script>
-import { parseTime } from '@/utils'
+import { parseTime, humanSize } from '@/utils'
 export default {
 
   data() {
     return {
       date: '',
-      timestamp: ''
+      timestamp: '',
+      bytes: '',
+      bytesUnit: 1,
+      humanSize: ''
     }
   },
   computed: {
@@ -28,6 +41,10 @@ export default {
   methods: {
     timestampToDate() {
       this.date = parseTime(this.timestamp)
+    },
+    bytesToHumanSize() {
+      console.log(this.bytes * this.bytesUnit > 9007199254740992)
+      this.humanSize = humanSize(this.bytes * this.bytesUnit)
     }
   }
 }
