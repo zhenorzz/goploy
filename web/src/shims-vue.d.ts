ff79064b23973041fd77ded43273a5808877d666
zhenorzz <zhenorzz@gmail.com>
2022-04-08 16:14:03 +0800

    U declare svgids

--- web/src/shims-vue.d.ts
+++ web/src/shims-vue.d.ts
@@ -2,3 +2,8 @@ declare module 'path-browserify' {
   import path from 'path'
   export default path
 }
+
+declare module 'virtual:svg-icons-names' {
+  const svgIds: string[]
+  export default svgIds
+}
