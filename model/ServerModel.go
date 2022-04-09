b7ec0b23a6aa1ecd92db852085eac6d0a35c1511
zhenorzz <zhenorzz@gmail.com>
2022-04-09 21:53:37 +0800

    A GPLv3

--- model/ServerModel.go
+++ model/ServerModel.go
@@ -1,3 +1,7 @@
+// Copyright 2022 The Goploy Authors. All rights reserved.
+// Use of this source code is governed by a GPLv3-style
+// license that can be found in the LICENSE file.
+
 package model
 
 import (
