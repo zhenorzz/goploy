a8d0df30c32c7ef69dd8b3c55fdaa69abfffc57c
zhenorzz <zhenorzz@gmail.com>
2022-11-23 16:58:30 +0800

    U refactor

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ read x
 
 if [[ $x =~ ^[1-9].[0-9]+.[0-9]+$ ]]
 then
-  sed -i -e "s/const appVersion = \"[0-9].[0-9]\+.[0-9]\+\"/const appVersion = \"$x\"/g" main.go
+  sed -i -e "s/const appVersion = \"[0-9].[0-9]\+.[0-9]\+\"/const appVersion = \"$x\"/g" cmd/server/main.go
   sed -i -e "s/'[0-9].[0-9]\+.[0-9]\+'/'$x'/g" model/sql/goploy.sql
   sed -i -e "s/GOPLOY_VER=v[0-9].[0-9]\+.[0-9]\+/GOPLOY_VER=v$x/g" docker/Dockerfile
 fi
@@ -23,10 +23,10 @@ fi
 
 echo "Building goploy";
 
-env GOOS=linux go build -o goploy main.go
+env GOOS=linux go build -o goploy cmd/server/main.go
 
-env GOOS=darwin go build -o goploy.mac main.go
+env GOOS=darwin go build -o goploy.mac cmd/server/main.go
 
-env GOOS=windows go build -o goploy.exe main.go
+env GOOS=windows go build -o goploy.exe cmd/server/main.go
 
 
